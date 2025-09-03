6547ed2404e246366f2cb66eed2ac846f9b628c6
kirillaowens <workmailk3@gmail.com>
2025-09-03 04:03:58 +0300

    finish recipe card

--- src/Components/MealRecipe.jsx
+++ src/Components/MealRecipe.jsx
@@ -1,11 +1,101 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import {
+  Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material';
 
 function MealRecipe({ infoMeal }) {
+  const getEmbedUrl = (url) => {
+    if (!url) return null;
+    return url.replace('watch?v=', 'embed/');
+  };
+
   return (
-    <Box>
-      <Typography variant="h5">{infoMeal.strMeal}</Typography>
-    </Box>
+    <Card sx={{ width: '50vw', my: 5, mx: 'auto' }}>
+      <CardContent>
+        <Typography variant="h3">{infoMeal.strMeal}</Typography>
+        {infoMeal.strArea && <Typography variant="body1">{infoMeal.strArea}</Typography>}
+        {infoMeal.strTags && <Typography variant="body1">{infoMeal.strTags}</Typography>}
+      </CardContent>
+
+      <CardMedia
+        component="img"
+        alt={infoMeal.strMeal}
+        image={infoMeal.strMealThumb}
+        sx={{ objectFit: 'cover', width: '70%', borderRadius: 5, m: 'auto' }}
+      />
+
+      <Divider sx={{ my: 2, width: '90%', mx: 'auto' }} />
+
+      <TableContainer component={Paper} sx={{ width: '90%', mx: 'auto' }}>
+        <Table aria-label="ingredients table">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <b>Ingredient</b>
+              </TableCell>
+              <TableCell>
+                <b>Measure</b>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.keys(infoMeal).map((key, idx) => {
+              if (key.includes('Ingredient') && infoMeal[key]) {
+                return (
+                  <TableRow key={idx}>
+                    <TableCell>{infoMeal[key]}</TableCell>
+                    <TableCell>{infoMeal[`strMeasure${key.slice(13)}`]}</TableCell>
+                  </TableRow>
+                );
+              }
+              return null;
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Divider sx={{ my: 2, width: '90%', mx: 'auto' }} />
+
+      {infoMeal.strYoutube && (
+        <Box
+          sx={{
+            position: 'relative',
+            paddingTop: '56.25%',
+            width: '90%',
+            mx: 'auto',
+            mb: 2,
+          }}
+        >
+          <Box
+            component="iframe"
+            src={getEmbedUrl(infoMeal.strYoutube)}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            sx={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              border: 0,
+              borderRadius: 3,
+            }}
+          />
+        </Box>
+      )}
+    </Card>
   );
 }
 
--- src/Pages/Recipe.jsx
+++ src/Pages/Recipe.jsx
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
 import { getMealById } from '../api';
+import Preloader from '../Components/Preloader';
 import MealRecipe from '../Components/MealRecipe';
 
 function Recipe() {
   const { id } = useParams();
-  const [infoMeal, setInfoMeal] = useState([]);
+  const [infoMeal, setInfoMeal] = useState(null);
 
   useEffect(() => {
     getMealById(id).then((data) => setInfoMeal(data.meals[0]));
   }, [id]);
 
-  return <MealRecipe infoMeal={infoMeal} />;
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {!infoMeal ? <Preloader /> : <MealRecipe infoMeal={infoMeal} />}
+    </Box>
+  );
 }
 
 export default Recipe;
